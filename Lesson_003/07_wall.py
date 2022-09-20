e23fa64e465e6f134d65110ed7740fb49ceba677
Cas <easypowerbang@gmail.com>
2022-09-21 07:09:45 +0900

    completed lesson_003_07.

--- Lesson_003/07_wall.py
+++ Lesson_003/07_wall.py
@@ -7,27 +7,27 @@ import simple_draw as sd
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
+sd.resolution = (1000, 500)
+point_x = 0
+point_y = 0
+brick_wight = 100
+brick_height = 50
+row_brick_number = 10
+row_number = 10
+red = [127, 0, 0]
 
-# point_1 = sd.get_point(0,0)      #(100, 0)
-# point_2 = sd.get_point(100, 0)   #(200, 0)
-# point_3 = sd.get_point(100, 50)  #(200, 50)
-# point_4 = sd.get_point(0, 50)    #(100,50)
-
-x1 = 0
-x2 = 100
-for x in range(10):
-    point_1 = sd.get_point(x1, 0)
-    point_2 = sd.get_point(x2, 0)
-    point_3 = sd.get_point(x2, 50)
-    point_4 = sd.get_point(x1, 50)
-    for x1 in range(1):
-        x1 += 100
-        x2 += 100
-        sd.line(point_1, point_2, width=1)
-        sd.line(point_2, point_3, width=1)
-        sd.line(point_3, point_4, width=1)
-        sd.line(point_4, point_1, width=1)
-
+for current_row in range(row_number):
+    offset = 50 if current_row % 2 == 0 else 0
+    for current_brick in range(row_brick_number):
+        left_bottom_x = point_x + offset + brick_wight * current_brick
+        left_bottom_y = point_y + brick_height * current_row
+        left_bottom = sd.get_point(left_bottom_x, left_bottom_y)
+        right_top_x = left_bottom_x + brick_wight
+        right_top_y = left_bottom_y + brick_height
+        right_top = sd.get_point(right_top_x, right_top_y)
+        sd.rectangle(left_bottom, right_top, color=red, width=3)
 
 
 sd.pause()
+y2 = 0
+number = 0
