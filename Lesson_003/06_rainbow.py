89e34e8583c7854efb686c56d853372a29da15c4
Cas <easypowerbang@gmail.com>
2022-09-20 20:36:03 +0900

    True lesson_003_06.

--- Lesson_003/06_rainbow.py
+++ Lesson_003/06_rainbow.py
@@ -18,25 +18,6 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 #     x2 += 10
 #     sd.line(sd.get_point(x1, 50), sd.get_point(x2, 350), color=rainbow_colors[i], width=4)
 
-
-
-for x in range(50, 121, 10):
-    point_1 = sd.get_point(x,50)
-    for x1 in range(350, 421, 10):
-        point_2 = sd.get_point(x1,450)
-        sd.line(point_1, point_2, width=5)
-
-
-# for x1 in range(50, 351, 50):
-#     for y1 in range(50, 351, 50):
-#         for x2 in range(350, 701, 50):
-#             for y2 in range(450, 801, 50):
-#                 point_1 = sd.get_point(x1, y1)
-#                 point_2 = sd.get_point(x2, y2)
-#                 sd.line(point_1, point_2, width=4)
-
-
-
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
