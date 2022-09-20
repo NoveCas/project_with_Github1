c7a6ee806c0bc57ef3226453a88e85fb2dc312cf
NoveCas <newvisionkonst@gmail.com>
2022-09-20 19:57:04 +0900

    completed lesson_003_06 and TODO lesson_003_07.

--- Lesson_003/06_rainbow.py
+++ Lesson_003/06_rainbow.py
@@ -4,15 +4,31 @@
 
 import simple_draw as sd
 
+sd.resolution = (600, 600)
+
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+
+# x1 = 50
+# x2 = 350
+# for i in range(len(rainbow_colors)):
+#     x1 += 10
+#     x2 += 10
+#     sd.line(sd.get_point(x1, 50), sd.get_point(x2, 350), color=rainbow_colors[i], width=4)
+
+
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+
+
+radius = 350
+for color in rainbow_colors:
+    radius += 25
+    point = sd.get_point(300, -50)
+    sd.circle(point, radius=radius, color=color, width=20)
 
 sd.pause()
--- Lesson_003/07_wall.py
+++ Lesson_003/07_wall.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
 # TODO здесь ваш код
 
-simple_draw.pause()
+# point_1 = sd.get_point(0,0)      #(100, 0)
+# point_2 = sd.get_point(100, 0)   #(200, 0)
+# point_3 = sd.get_point(100, 50)  #(200, 50)
+# point_4 = sd.get_point(0, 50)    #(100,50)
+
+x1 = 0
+x2 = 100
+for x in range(10):
+    point_1 = sd.get_point(x1, 0)
+    point_2 = sd.get_point(x2, 0)
+    point_3 = sd.get_point(x2, 50)
+    point_4 = sd.get_point(x1, 50)
+    for x1 in range(1):
+        x1 += 100
+        x2 += 100
+        sd.line(point_1, point_2, width=1)
+        sd.line(point_2, point_3, width=1)
+        sd.line(point_3, point_4, width=1)
+        sd.line(point_4, point_1, width=1)
+
+
+
+sd.pause()
