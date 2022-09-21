4a77de32cb582272270c82168a1d1fc225841812
Cas <easypowerbang@gmail.com>
2022-09-22 00:01:13 +0900

    completed lesson_003_08, need to add color in def.

--- Lesson_003/08_smile.py
+++ Lesson_003/08_smile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
@@ -9,5 +9,28 @@ import simple_draw
 # Вывести 10 смайликов в произвольных точках экрана.
 
 # TODO здесь ваш код
+def smile_draw(x, y):
+    point_smile = sd.get_point(x, y)
+    color = sd.random_color()
+    sd.circle(center_position=point_smile, radius=50, color=color, width=3)
 
-simple_draw.pause()
+    point_eye_1 = sd.get_point(x - 15, y + 15)
+    sd.circle(center_position=point_eye_1, radius=5, color=color, width=1)
+
+    point_eye_2 = sd.get_point(x + 15, y + 15)
+    sd.circle(center_position=point_eye_2, radius=5, color=color, width=1)
+
+    p1 = sd.get_point(x - 30, y - 10)
+    p2 = sd.get_point(x - 5, y - 25)
+    p3 = sd.get_point(x + 5, y - 25)
+    p4 = sd.get_point(x + 30, y - 10)
+    point_list = [p1, p2, p3, p4]
+    sd.lines(point_list, color=color, width=1)
+
+
+smile_draw(100, 100)
+
+
+
+
+sd.pause()
